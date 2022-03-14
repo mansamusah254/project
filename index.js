d32be1f401d4aeb8a3dfbe78a4d70b69d9234d1f
mansamusah254 <mansamusah830@gmail.com>
2022-03-14 11:13:20 +0300

    adjustments and additions to all files

--- index.js
+++ index.js
@@ -1,3 +1,77 @@
 $("col-md-4").click(function(){
     $("col-md-4").hide(1000);
   });
+
+   $(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
+    });
+  });
+
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product .title").show('1500');
+    });
+    $("#product .title").click(function(){
+      $("#product .title").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#overlay1").show();
+    }).mouseout(function(){
+      $("#overlay1").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work4").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+    $("#work6").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+    $("#work7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+    $("#work8").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
